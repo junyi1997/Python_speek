94dd9fc4aaae88af16fc68a9d60ef928be9f22be
junyi <q5896799@gmail.com>
2019-03-29 09:56:01 +0800

    'debug'

---
+++ 123.py
@@ -0,0 +1,37 @@
+'''
+import speech_recognition as sr
+
+def bot_listen():
+    recog=sr.Recognizer()
+    with sr.Microphone() as source:
+        audioData = recog.listen(source)
+        try:
+            text=recog.recognizer_google(audioData,language='zh-tw')
+            return text
+        except:
+            return '聽不懂'
+question=bot_listen()
+print(question)            
+'''
+
+import speech_recognition as sr
+
+#obtain audio from the microphone
+r=sr.Recognizer() 
+with sr.Microphone() as source:
+    print("Please wait. Calibrating microphone...") 
+    #listen for 5 seconds and create the ambient noise energy level 
+    r.adjust_for_ambient_noise(source, duration=5) 
+    print("Say something!")
+    audio=r.listen(source)
+
+# recognize speech using Google Speech Recognition 
+try:
+    print("Google Speech Recognition thinks you said:")
+    print(r.recognize_google(audio, language="zh-TW"))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("No response from Google Speech Recognition service: {0}".format(e))
+
+#上面程式是利用 SpeechRecognition 模組中的 recognixe_google() 函數透過 Google 語音辨識 API 來將麥克風收到的語音物件 audio 辨識成指定語系的文字 :
